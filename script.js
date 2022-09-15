0e3a4cf7356314371d801b4f58b1079c6b0a0f3f
unknown <hasinadilruba2021@gmail.com>
2022-09-15 12:51:16 +0200

    count-down added

--- script.js
+++ script.js
@@ -108,10 +108,10 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
  
     // finished timer
-    if (count < 0) {
+    if (count == 0) {
      
       
       //-------------- START TYPING -----------------
